e54dd0b844e2bc5700267bfbdeaff33b26b54327
RaitP <rait.pommer@ut.ee>
2022-12-12 16:37:12 +0200

    4.5 hour set-up finished

---
+++ database.js
@@ -0,0 +1,11 @@
+ // database.js
+ const Pool = require('pg').Pool;
+ const pool = new Pool({
+     user: "postgres",
+     password: "123",
+     database: "testWad",
+     host: "localhost",
+     port: "5432"
+ });
+ 
+ module.exports = pool;
